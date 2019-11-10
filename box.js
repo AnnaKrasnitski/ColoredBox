e8b31aa5d4609ddec72d08f7a74c1bab7c8c2375
iProsisTomer <47002031+iProsisTomer@users.noreply.github.com>
2019-11-10 16:51:37 +0200

    Add files via upload

--- box.js
+++ box.js
@@ -9,7 +9,12 @@
 			border-style: solid;
 			display: block;
 		} 
-		</style> 
+    </style> 
+    <svg viewBox="0 0 10 10" xmlns="http://www.w3.org/2000/svg">
+  <rect width="10" height="10">
+    <animate attributeName="rx" values="0;5;0" dur="10s" repeatCount="indefinite" />
+  </rect>
+</svg>
 	`;
 
   class Box extends HTMLElement {
